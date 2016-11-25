28eb5e41fe3ff08f487bb9778165e1c664498488
Ziqiang Huang <huangziqiang@ziqiang.local>
2016-11-25 14:29:49 -0800

    Initial Commit

---
+++ CongressAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CongressAPI
+//
+//  Created by Ziqiang Huang on 11/25/16.
+//  Copyright © 2016 Ziqiang Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
